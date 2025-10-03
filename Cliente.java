5763d8da7088df9406645d236dbef3d25463d2a1
4nnaBalmiz4 <anna.soares@discente.uenp.edu.br>
2025-10-03 14:34:20 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -3,10 +3,55 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author AnnaLauraBalmizaSoar
  */
-public class Cliente {
+public class Leilao implements ILeilao {
+    
+    private List<ICliente> clientes; 
+    private Integer lanceMin; 
+    
+    public Leilao(){
+        clientes = new ArrayList(); 
+        lanceMin = 0;
+    }
+    
+    @Override
+    public void adicionarCliente(ICliente ic) {
+        clientes.add(ic);
+    }
+
+    @Override
+    public void removerCliente(ICliente ic) {
+        clientes.remove(ic);
+    }
+
+    @Override
+    public void notificarClientes(){
+        int maiorLance = 0;
+        for (ICliente i : clientes){
+            i.atualizar(lanceMin);
+            int lance = i.atualizar(lanceMin);
+            if(lance > maiorLance){
+                maiorLance = lance;
+            }
+        }
+    }
+
+    public List<ICliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<ICliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void setComprador(String comprador) {
+        this.lanceMin = lanceMin;
+    }
     
 }
