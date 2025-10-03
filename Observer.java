1dbb38283613943d155d5eb59767c7bd359ef6ce
4nnaBalmiz4 <anna.soares@discente.uenp.edu.br>
2025-10-03 14:35:16 -0300

    Update Observer.java

--- Observer.java
+++ Observer.java
@@ -7,6 +7,23 @@
  *
  * @author AnnaLauraBalmizaSoar
  */
+
 public class Observer {
-    
+    public static void main(String[] args) {
+        System.out.println("Demonstração do padrão observer");
+        ICliente cliente1 = new Cliente("Petrus");
+        ICliente cliente2 = new Cliente("Julius");
+        ICliente cliente3 = new Cliente("Aloki");
+        ICliente cliente4 = new Cliente("Totus");
+        
+        
+        Leilao vendedor = new Leilao();
+        vendedor.adicionarCliente(cliente1);
+        vendedor.adicionarCliente(cliente2);
+        vendedor.adicionarCliente(cliente3);
+        vendedor.adicionarCliente(cliente4);
+        
+        vendedor.setComprador("Adubo NPK com 5% de desconto");
+        vendedor.notificarClientes();
+    }
 }
